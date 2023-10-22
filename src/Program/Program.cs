4a41c2948ba931a6f17ea48a8bd41ee31253696b
MHC2005 <mhcedres@gmail.com>
2023-10-22 13:32:56 -0300

    ejercicio 1

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+
+        PictureProvider provider = new PictureProvider();
+        IPicture picture = provider.GetPicture(@"luke.jpg");
+
+        IFilter filter_grey = new FilterGreyscale(); 
+        IFilter filter_negative = new FilterNegative();
+
+        IPipe pipe1  = new PipeSerial(filter_grey,new PipeNull());
+        IPipe pipe2  = new PipeSerial(filter_negative,new PipeNull());
+
+        pipe1.Send(picture);
+        IPicture image = pipe2.Send(picture);
+
+        PictureProvider provider2 = new PictureProvider();
+        provider.SavePicture(image, @"luke2.jpg");
         }
     }
 }
